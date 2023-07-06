2995a48bcc1d7d971914ffbe425845eca9a2afe0
dmgasques <dmgasques@gmail.com>
2023-07-06 16:27:00 -0300

    Add product preview modal functionality and ui

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -2,11 +2,12 @@
 
 import { Product } from '@/types';
 import Image from 'next/image';
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import IconButton from './icon-button';
 import { Expand, ShoppingCart } from 'lucide-react';
 import Currency from './currency';
 import { useRouter } from 'next/navigation';
+import usePreviewModal from '@/hooks/use-preview-modal';
 
 type ProductCardProps = {
   data: Product;
@@ -14,9 +15,16 @@ type ProductCardProps = {
 
 export default function ProductCard({ data }: ProductCardProps) {
   const router = useRouter();
+  const previewModal = usePreviewModal();
 
   const handleClick = () => {
     router.push(`/product/${data.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.stopPropagation();
+
+    previewModal.onOpen(data);
   };
 
   return (
@@ -34,7 +42,7 @@ export default function ProductCard({ data }: ProductCardProps) {
         <div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5'>
           <div className='flex gap-x-6 justify-center'>
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className='text-gray-600' />}
             />
             <IconButton
